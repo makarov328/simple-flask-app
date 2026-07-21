62c602f8717d9c5e2f7d26669030b58d3c7dfad9
john <theant109@gmail.com>
2026-07-21 21:56:37 +0700

    ci: Add terraform and initial CD workflow 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,14 +11,42 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
+resource "aws_iam_role" "apprunner_ecr_access" {
+  name = "apprunner-ecr-access-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "build.apprunner.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "apprunner_ecr_access_policy" {
+  role       = aws_iam_role.apprunner_ecr_access.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+}
+
 resource "aws_apprunner_service" "staging" {
   service_name = "simple-flask-app-staging"
 
-
   source_configuration {
+    authentication_configuration {
+      access_role_arn = aws_iam_role.apprunner_ecr_access.arn
+    }
+
     image_repository {
-      image_identifier = var.ecr_image_identifier
+      image_identifier      = var.ecr_image_identifier
       image_repository_type = "ECR"
+      image_configuration {
+        port = "8080"
+      }
     }
     auto_deployments_enabled = false
   }
